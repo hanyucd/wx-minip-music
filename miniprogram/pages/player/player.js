ebc54904f94d3ef8e4431f8c72052d4ce371d9fc
hanyucd <1807003141@qq.com>
2019-12-21 19:56:22 +0800

    feat: 完成播放器页面播放歌曲功能.

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -2,15 +2,16 @@
 let currentMusicIndex = 0; // 当前播放歌曲的索引
 let globalMusicList = []; // 存储全局的歌单歌曲列表
 let musiclist = []; // 歌单歌曲列表
-
-const app = getApp() // 获取全局app实例
+const backgroundAudioManager = wx.getBackgroundAudioManager(); // 获取全局唯一的背景音频管理器
+const app = getApp(); // 获取全局app实例
 
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    picUrl: '' // 歌曲的封面图
+    picUrl: '', // 歌曲的封面图
+    isPlaying: false // 歌曲是否播放中，默认 false 没有播放
   },
 
   /**
@@ -25,6 +26,8 @@ Page({
    * @param {String} musicId 
    */
   _loadMusicDetail(musicId) {
+    backgroundAudioManager.stop();
+    
     globalMusicList = app.globalData.musicList;
     // 拿去全局的歌曲列表，将歌单所有歌曲信息储存在Storage
     wx.setStorageSync('musiclist', globalMusicList);
@@ -32,12 +35,11 @@ Page({
     musiclist = wx.getStorageSync('musiclist');
 
     let musicInfo = musiclist[currentMusicIndex];
-    wx.setNavigationBarTitle({ title: musicInfo.name });
+    wx.setNavigationBarTitle({ title: musicInfo.name }); // 动态设置当前页面的标题
     this.setData({
       picUrl: musicInfo.al.picUrl
     });
-    // console.log(musicInfo);
-    
+    wx.showLoading({ title: '歌曲加载中...' });
     // 获取歌曲播放链接
     wx.cloud.callFunction({
       name: 'music',
@@ -46,7 +48,40 @@ Page({
         $url: 'musicUrl'
       }
     }).then(res => {
-      console.log(res);
+      const result = res.result;
+      console.log(result);
+
+      backgroundAudioManager.src = result.data[0].url;
+      backgroundAudioManager.title = musicInfo.name;
+      backgroundAudioManager.coverImgUrl = musicInfo.al.picUrl;
+      backgroundAudioManager.singer = musicInfo.ar[0].name;
+      backgroundAudioManager.epname = musicInfo.ar.name;
+
+      this.setData({ isPlaying: true });
+      wx.hideLoading();
     });
+  },
+  /**
+   * 点击播放按钮切换播放 和 暂停
+   */
+  togglePlay() {
+    this.data.isPlaying ? backgroundAudioManager.pause() : backgroundAudioManager.play();
+    this.setData({ isPlaying: !this.data.isPlaying });
+  },
+  /**
+   * 上一首
+   */
+  prevMusic() {
+    --currentMusicIndex;
+    currentMusicIndex = currentMusicIndex < 0 ? musiclist.length - 1 : currentMusicIndex;
+    this._loadMusicDetail(musiclist[currentMusicIndex].id);
+  },
+  /**
+   * 下一首
+   */
+  nextMusic() {
+    ++currentMusicIndex;
+    currentMusicIndex = currentMusicIndex > musiclist.length ? 0 : currentMusicIndex;
+    this._loadMusicDetail(musiclist[currentMusicIndex].id);
   }
 });
